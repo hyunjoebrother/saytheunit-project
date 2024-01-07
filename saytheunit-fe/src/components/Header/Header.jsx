6a642feab65a748870868582d078b4045fda85b5
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-07 18:31:37 +0900

    feat: implement Mobile Toggle UI

--- saytheunit-fe/src/components/Header/Header.jsx
+++ saytheunit-fe/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="w-full h-20 bg-red-200 text-2xl text-white flex items-center justify-center">
+    <header className="w-full 2xs:h-12 xs:h-12 2sm:h-14 sm:h-14 tb:h-20 bg-red-200 text-2xl text-white flex items-center justify-center">
       LOGO
     </header>
   );
