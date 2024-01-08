9ad93955a8f326e4e7f1d60299689f8cc5c27d7f
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-08 16:41:30 +0900

    feat: implement total Layout UI

--- saytheunit-fe/src/components/CountingCard/CountingCard.jsx
+++ saytheunit-fe/src/components/CountingCard/CountingCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './index.css';
 
-const TipCard = () => {
+const CountingCard = () => {
   const [count, setCount] = useState(0);
   const [isAnimating, setIsAnimating] = useState(true);
 
@@ -42,19 +42,20 @@ const TipCard = () => {
     <section className="w-full 2sm:w-[326px] sm:w-[480px] tb:w-[560px] lg:w-[600px] flex h-auto 2xs:rounded-[10px] rounded-2xl bg-[#c9f4ed] 2xs:px-4 xs:px-2 2sm:px-4 px-10 py-4">
       <div className="flex flex-col gap-2 m-auto text-center items-center justify-center">
         <p className="2xs:text-lg text-xl font-bold text-[#00dcb6]">
-          {count}
+          "{count}
           <span className="ml-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
             개의 유닛
           </span>
+          "
         </p>
-        <p className="font-scriptFont font-bold tip-text 2xs:text-[10px] xs:text-sm 2sm:text-sm text-black">
+        <p className="font-scriptFont tip-text 2xs:text-[10px] xs:text-sm 2sm:text-sm text-black">
           세븐틴 멤버 13명을 기준으로 2~12명 조합을 만든다고 가정했을 때,
           <br />
-          발생할 수 있는 조합의 수는 다음과 같다. [위버스 메거진]
+          발생할 수 있는 조합의 수는 다음과 같다. [위버스 매거진]
         </p>
       </div>
     </section>
   );
 };
 
-export default TipCard;
+export default CountingCard;
