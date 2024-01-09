1b6d5ea7f786bb7671c73511d1ec5fa4a8f9dc40
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-09 23:36:37 +0900

    feat: setting HITs Counter by HITS

--- saytheunit-fe/src/components/HitCard/HitCard.jsx
+++ saytheunit-fe/src/components/HitCard/HitCard.jsx
@@ -1,44 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import carrot from '../../assets/images/carrot-icon.png';
 import './index.css';
 
 const HitCard = () => {
-  const [count, setCount] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(true);
-
-  const end = 8177;
-  const duration = 1000;
-
-  const frameRate = 1000 / 60;
-  const totalFrame = Math.round(duration / frameRate);
-
-  const easeOutExpo = number => {
-    return number === 1 ? 1 : 1 - Math.pow(2, -10 * number);
-  };
-
-  useEffect(() => {
-    let currentFrame = 0;
-
-    const animateCount = () => {
-      const progress = easeOutExpo(currentFrame / totalFrame);
-      const newCount = Math.round(end * progress);
-
-      setCount(newCount);
-
-      currentFrame++;
-
-      if (currentFrame <= totalFrame) {
-        requestAnimationFrame(animateCount);
-      } else {
-        setIsAnimating(false);
-      }
-    };
-
-    if (isAnimating) {
-      requestAnimationFrame(animateCount);
-    }
-  }, [end, isAnimating, totalFrame]);
-
   return (
     <section className="w-full h-auto 2xs:rounded-[10px] xs:rounded-[12px] rounded-2xl bg-white text-black 2xs:px-6 xs:px-6 2sm:px-6 px-10 py-6 tb:py-8 lg:py-10">
       <div className="flex flex-col gap-2 m-auto text-center items-center justify-center">
@@ -49,13 +13,17 @@ const HitCard = () => {
             className="flex m-auto mb-3 tb:mb-5 lg:mb-6 w-20 tb:w-24 lg:w-28"
           />
           <p>
-            <span className="mx-1 2xs:text-[10px] xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
-              방문한 당근
+            <span className="2xs:text-[10px] xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
+              방문자 수
             </span>
-            {count}
-            <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
-              명
-            </span>
+            <div className="w-full flex m-auto items-center justify-center 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
+              <a href="https://hits.sh/saytheunit.com/">
+                <img
+                  alt="Hits"
+                  src="https://hits.sh/saytheunit.com.svg?label=HITs&color=ffffff&labelColor=fda73e"
+                />
+              </a>
+            </div>
           </p>
         </div>
       </div>
