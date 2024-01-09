db0ba4d85db101d029d6e7a4b4fd03cc56eeab15
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-09 12:36:46 +0900

    feat: implement SnapshootPDF Card UI

--- saytheunit-fe/src/components/HitCard/HitCard.jsx
+++ saytheunit-fe/src/components/HitCard/HitCard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import carrot from '../../assets/images/carrot-icon.png';
 import './index.css';
 
 const HitCard = () => {
@@ -41,27 +42,18 @@ const HitCard = () => {
   return (
     <section className="w-full 2sm:w-[326px] sm:w-[480px] tb:w-[560px] lg:w-[600px] h-auto 2xs:rounded-[10px] xs:rounded-[12px] rounded-2xl bg-white text-black 2xs:px-4 xs:px-2 2sm:px-4 px-10 py-6">
       <div className="flex flex-col gap-2 m-auto text-center items-center justify-center">
-        <p className="2xs:text-[20px] text-[24px] font-extrabold text-[#00dcb6]">
-          <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold">
-            현재 방문한 당근들
-          </span>
-          <span className="font-sayingFont">"</span>
-          {count}
-          <span className="font-sayingFont">"</span>
-          <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold">
-            명
-          </span>
-        </p>
-        <p className="tip-text 2xs:text-[10px] xs:text-sm 2sm:text-sm">
-          내가 한 번 지어볼까?!
-          <a
-            className="no-underline text-[#00dcb6] font-bold"
-            href="https://magazine.weverse.io/article/view?lang=ko&num=422"
-          >
-            HIT
-          </a>
-          <br />
-        </p>
+        <div className="w-full flex-inline flex-col m-auto items-center justify-center 2xs:text-[16px] text-[20px] font-extrabold text-[#fda73e]">
+          <img src={carrot} alt="" className="flex m-auto mb-3 2sm:w-20" />
+          <p>
+            <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
+              방문한 당근
+            </span>
+            {count}
+            <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold text-black">
+              명
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
--- saytheunit-fe/src/components/SnapshootCard/SnapshootCard.jsx
+++ saytheunit-fe/src/components/SnapshootCard/SnapshootCard.jsx
@@ -3,24 +3,24 @@ import './index.css';
 
 const SnapshootCard = () => {
   return (
-    <section className="w-full h-auto bg-[#fcabab] text-white 2xs:px-4 xs:px-2 2sm:px-4 px-10 py-6">
-      <div className="flex flex-col gap-2 m-auto text-center items-center justify-center">
-        <p className="2xs:text-[20px] text-[24px] font-extrabold text-[#a03333]">
-          <span className="mx-1 2xs:text-xs xs:text-xs 2sm:text-xs text-sm font-semibold">
-            편하고 빠르게 악플 고소를 하고 싶다면?
+    <section className="w-full h-auto bg-[#A80603] text-white 2xs:px-4 xs:px-2 2sm:px-4 px-10 py-6">
+      <div className="flex flex-col gap-3 m-auto text-center items-center justify-center">
+        <div className="tip-text 2xs:text-[20px] text-[24px] font-extrabold">
+          <span className="x-1 2xs:text-xs xs:text-xs 2sm:text-base text-base font-semibold text-white">
+            쉽고, 편하고, 한번에 PDF 증거자료를 수집하고 싶다면?
           </span>
-        </p>
-        <p className="tip-text 2xs:text-[10px] xs:text-sm 2sm:text-sm">
-          내가 한 번 지어볼까?!
+        </div>
+        <div className="2xs:text-[10px] xs:text-sm 2sm:text-sm">
           <a
-            className="no-underline text-[#a03333] font-bold"
-            href="https://magazine.weverse.io/article/view?lang=ko&num=422"
+            className="no-underline font-bold"
+            href="https://mei-day.notion.site/mei-day/SnapshootPDF-047d13b7a868424eb37fa449e2cc5725"
           >
-            SnapshootPDF
+            <button className="w-auto h-auto px-3 py-2 bg-white text-[#A80603] rounded-md font-extrabold">
+              SnapshootPDF 사용해보기
+            </button>
           </a>
-          <br />
-          대충 로로 사진
-        </p>
+        </div>
+        <div className="mt-0 2sm:text-[10px]">제가 개발했습니다.</div>
       </div>
     </section>
   );
