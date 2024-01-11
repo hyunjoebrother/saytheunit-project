9f2ad84d6c33e2e9c0b07c0aabb1af7e4c0673a5
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-11 10:54:42 +0900

    feat: add 404 Error page && Route

--- saytheunit-fe/src/components/App.js
+++ saytheunit-fe/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main/Main';
+import Error404 from '../pages/Error404/Error404';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path={'*'} element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ saytheunit-fe/src/pages/Error404/Error404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import errorImg from '../../../public/service-update-page.jpg';
+
+const Error404 = () => {
+  return <img src={errorImg} alt="Say the 점검" />;
+};
+
+export default Error404;
