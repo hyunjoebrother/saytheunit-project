d796d0767bd9ab669a328cccc3a7c96f602062e4
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-01-12 13:36:49 +0900

    fix: preloading img && font

--- saytheunit-fe/src/components/App.js
+++ saytheunit-fe/src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main/Main';
 import Error404 from '../pages/Error404/Error404';
+import preloadBg from '../assets/images/diamond-wallbg.png';
+import preloadAlbum from '../assets/images/svt-total-bg-frame.jpg';
 
 const App = () => {
+  useEffect(() => {
+    const preloadImage = new Image();
+    preloadImage.src = preloadBg;
+    preloadImage.src = preloadAlbum;
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
